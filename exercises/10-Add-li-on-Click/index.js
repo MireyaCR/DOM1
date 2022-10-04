3103864f0b39626861389d8b61a72aee4cf0d7ac
MireyaCR <mireya.mcr@gmail.com>
2022-10-04 15:20:50 +0000

    Dom1

--- exercises/10-Add-li-on-Click/index.js
+++ exercises/10-Add-li-on-Click/index.js
@@ -1,5 +1,9 @@
 let button = document.querySelector("#superDuperButton");
 button.addEventListener("click", function() {
 	//your code here
-
+	var list = document.querySelector("#myList");
+	var elem = document.createElement("li");
+	elem.innerHTML="Forth element";
+	list.appendChild(elem);
+	
 });
