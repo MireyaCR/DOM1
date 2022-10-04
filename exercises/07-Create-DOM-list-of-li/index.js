79f3370a9872562f340ccad12da0d2a13fa569fd
MireyaCR <mireya.mcr@gmail.com>
2022-10-04 09:58:28 +0000

    Dom1

--- exercises/07-Create-DOM-list-of-li/index.js
+++ exercises/07-Create-DOM-list-of-li/index.js
@@ -1,6 +1,6 @@
-let beginning = "<ul>";
-let liststring = "";
-let ending = "</ul>";
+const beginning='<ul>'
+const liststring='<li>First Item</li><li>Second Item</li><li>Third Item</li>'
+const ending='</ul>'
 
 // do not modify after this line
 document.body.innerHTML = beginning + liststring + ending;
